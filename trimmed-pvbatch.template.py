58315922b5f15a5c73ab286c5f2c37863e501fd3
Nathaniel Hellabyte <nate@hellabit.es>
2015-04-03 15:00:15 -0700

    init

--- trimmed-pvbatch.template.py
+++ trimmed-pvbatch.template.py
@@ -11,10 +11,10 @@ inputpath="{{inputpath}}"
 inputfile="{{inputfile}}" 
 
 for i in range( {{begin}} , {{end}} ): 
-	binfile = '{}{}{:04d}.vtk'.format(inputpath, inputfile, i) 
+  binfile = '{}{}{:04d}.vtk'.format(inputpath, inputfile, i) 
   # Set up Frame
   # Render()
-	figfile = '{{outputfile}}{:04d}{{imageformat}}'.format(i) 
+  figfile = '{{outputfile}}{:04d}{{imageformat}}'.format(i) 
   # View
   # WriteImage
   # Delete Frame
